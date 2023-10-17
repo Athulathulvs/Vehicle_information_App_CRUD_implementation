8a673e8dddfd526b8c3badede13544a8f3a5a0b7
Athulathulvs <athulvs999@gmail.com>
2023-10-17 22:54:38 +0530

    Create

---
+++ crud_realtime_admin/src/main/java/com/example/crud_realtime_admin/UploadActivity.kt
@@ -0,0 +1,48 @@
+package com.example.crud_realtime_admin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.crud_realtime_admin.databinding.ActivityUploadBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class UploadActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadBinding
+    private lateinit var databaseReference: DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        binding.saveButton.setOnClickListener {
+            val ownerName =binding.uploadOwnerName.text.toString()
+            val vehicleBrand =binding.uploadVehicleBrand.text.toString()
+            val vehicleRTO =binding.uploadVehicleRTO.text.toString()
+            val vehicleNumber =binding.uploadVehicleNumber.text.toString()
+
+            databaseReference = FirebaseDatabase.getInstance().getReference("Vehicle Information")
+            val vehicleData =VehicleData(ownerName, vehicleBrand, vehicleRTO, vehicleNumber)
+            //Primary key -> must be unique
+            databaseReference.child(vehicleNumber).setValue(vehicleData)
+                .addOnSuccessListener {
+                     binding.uploadOwnerName.text.clear()
+                    binding.uploadVehicleBrand.text.clear()
+                    binding.uploadVehicleRTO.text.clear()
+                    binding.uploadVehicleNumber.text.clear()
+
+                    Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+                    val intent =Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+            }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CRUDE_Realtime_App"
+include(":app")
+include(":crud_realtime_admin")
